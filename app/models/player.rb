6d075b0a6a9aad758fce6b33726c03d3b1af8635
Gia Jennings <gjennings.co@gmail.com>
2021-02-20 17:38:22 -0500

    Add Player validations

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,8 @@
 class Player < ActiveRecord::Base
     has_many :rosters
     has_many :teams, through: :rosters
+    validates :name, presence: true, uniqueness: true
+    validates :number, presence: true, uniqueness: true
+    validates :position, allow_nil: true
+    validates :rating, allow_nil: true
 end
