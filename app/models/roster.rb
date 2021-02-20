1cab37b4292d50af808196231b8f72e20b73e903
Gia Jennings <gjennings.co@gmail.com>
2021-02-20 18:12:35 -0500

    Update Roster season validation

--- app/models/roster.rb
+++ app/models/roster.rb
@@ -3,4 +3,5 @@ class Roster < ActiveRecord::Base
     belongs_to :team 
     validates :player, presence: true
     validates :team, presence: true
+    validates :season, presence: true, uniqueness: true
 end
--- db/migrate/20210220163316_create_teams.rb
+++ db/migrate/20210220163316_create_teams.rb
@@ -1,9 +1,9 @@
 class CreateTeams < ActiveRecord::Migration[5.2]
   def change
     create_table :teams do |t|
-      t.text :team_name
       t.text :public_key
       t.text :private_key
+      t.text :team_name
       t.references :coach, foreign_key: {to_table: :users, on_delete: :cascade}
       t.string :password_digest
       t.string :api_key
